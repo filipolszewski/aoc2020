dcd8b1cd8d3ed33ab06918a684a96e26f41edf5b
Filip Olszewski <filipolszew@o2.pl>
2020-12-01 08:12:11 +0100

    Day1 - both parts, part 1 optimised

--- day1/part1.py
+++ day1/part1.py
@@ -2,9 +2,23 @@ def main():
     """ Main program """
 
     with open("input.txt", 'r') as data:
-        lines = [line.strip() for line in data.readlines()]
-    print(lines)
-    return 0
+        report_lines = [int(line.strip()) for line in data.readlines()]
+
+    # part 1
+    needed_numbers = set()
+    for i, number in enumerate(report_lines):
+        if number in needed_numbers:
+            print(number * (2020 - number))
+        needed_numbers.add(2020 - number)
+
+    # part 2
+    for i in range(len(report_lines)):
+        for j in range(len(report_lines)):
+            for k in range(len(report_lines)):
+                if i != j != k and (report_lines[i] + report_lines[j] + report_lines[k]) == 2020:
+                    print(report_lines[i] * report_lines[j] * report_lines[k])
+                    return
+    return
 
 
 if __name__ == "__main__":
